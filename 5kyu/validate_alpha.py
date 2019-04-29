5dfedec8e4be50eabcc2438301bfc83adc39fee2
jm4ch4do <47991336+jm4ch4do@users.noreply.github.com>
2019-04-29 10:08:31 -0400

    Create validate_alpha.py

---
+++ 5kyu/validate_alpha.py
@@ -0,0 +1,37 @@
+# ----- Problem: Not very secure (5ku)
+# ----- URL: https://www.codewars.com/kata/526dbd6c8c0eb53254000110
+
+
+def validate_alpha(_password):
+    """
+    (string) -> (boolean)
+    :param _password: string with a password
+    :return: True if password it's only alphanumeric
+
+    :description:
+    Empty string is not a valid password
+    Allowed characters are uppercase/lowercase letters and digits
+    No whitespaces/underscore
+
+    :example:
+    validate_alpha('myPassword123') -> True
+    validate_alpha('my_pass') -> False
+    validate_alpha('mypass*') -> False
+
+    """
+    # empty input -> return False
+    if not _password:
+        return False
+
+    # if a characters is not letter or number -> return False
+    for ch in _password:
+        if ch.lower() not in 'abcdefghijklmnopqrstuvwxyz1234567890':
+            return False
+
+    # no problem -> return True
+    return True
+
+
+# ------------------------- MAIN CODE -------------------------
+result = validate_alpha('myPassword123')
+print(result)
